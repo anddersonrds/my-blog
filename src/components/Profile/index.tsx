d2776b16bb21bbd475ca0923f496803d1db3b1de
Anderson Rodrigues <anddersonrds@gmail.com>
2021-01-04 21:09:22 -0300

    add Profile component

---
+++ src/components/Profile/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './styles';
+
+const Profile = () => (
+  <S.Wrapper>
+    <h1>Anderson Rodrigues</h1>
+    <h2>Programador Frontend</h2>
+    <p>Gamer, programador, leitor e colecionador, padawan na arte de escrever artigos sobre programação.</p>
+  </S.Wrapper>
+);
+
+export default Profile;
