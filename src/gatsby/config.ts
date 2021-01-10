3c1f4aa567a2e149d2b5da6dfd0d6ed09268d34c
Anderson Rodrigues <anddersonrds@gmail.com>
2021-01-10 19:17:47 -0300

    Add Typescript

---
+++ src/gatsby/config.ts
@@ -0,0 +1,32 @@
+export default {
+  siteMetadata: {
+    title: `My blog`,
+    author: `@anddersonrds`,
+    position: `Frontend Developer`,
+    description: `A blog about frontend development and other cool stuff`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/../images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+  ],
+}
