ba0be9d72be7dafb18de6ba14ec599244c7153c3
Anderson Rodrigues <anddersonrds@gmail.com>
2021-01-10 21:43:26 -0300

    Config Prettier and Eslint

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 require('ts-node').register();
 
 module.exports = require('./src/gatsby/config');
--- src/gatsby/config.ts
+++ src/gatsby/config.ts
@@ -3,16 +3,17 @@ export default {
     title: `My blog`,
     author: `@anddersonrds`,
     position: `Frontend Developer`,
-    description: `A blog about frontend development and other cool stuff`,
+    description: `A blog about frontend development and other cool stuff`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-eslint`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/../images`,
-      },
+        path: `${__dirname}/../images`
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -25,8 +26,8 @@ export default {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-  ],
-}
+        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+      }
+    }
+  ]
+};
