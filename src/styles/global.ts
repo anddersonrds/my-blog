af037b396b519a5cc3ee91b0ccba28c02faaf380
Anderson Rodrigues <anddersonrds@gmail.com>
2021-01-12 23:32:13 -0300

    Add GlobalStyles

---
+++ src/styles/global.ts
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from 'styled-components';
+import theme from './theme';
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+    &::before,
+    &::after {
+      box-sizing: inherit;
+    }
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    font-family: ${theme.font.family};
+    font-size: ${theme.font.sizes.medium};
+    color: ${theme.colors.primary};
+    background: ${theme.colors.background};
+  }
+
+  img {
+    display: block;
+  	height: auto;
+  	width: 100%;
+  }
+`;
+
+export default GlobalStyles;
